7e652107b6e711f07dd1e5fdb4b991745a881dc2
Nish Tahir <nishtahir@outlook.com>
2016-04-14 00:10:48 +0300

    User ToString no longer includes package name

--- src/main/groovy/com/nishtahir/linkbait/model/User.groovy
+++ src/main/groovy/com/nishtahir/linkbait/model/User.groovy
@@ -3,9 +3,11 @@ package com.nishtahir.linkbait.model
 import com.j256.ormlite.field.DatabaseField
 import com.j256.ormlite.table.DatabaseTable
 import groovy.transform.Canonical
+import groovy.transform.ToString
 
 @Canonical
 @DatabaseTable
+@ToString(includePackage = false)
 class User {
 
     @DatabaseField(id = true)
