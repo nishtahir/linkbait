9995d0562d898bdf924ee177cf053874f0b70d9c
Nish Tahir <nishtahir@outlook.com>
2016-06-24 16:27:54 +0300

    Update booter

--- src/main/groovy/com/nishtahir/linkbait/core/util/Booter.groovy
+++ src/main/groovy/com/nishtahir/linkbait/core/util/Booter.groovy
@@ -7,39 +7,71 @@ import org.eclipse.aether.RepositorySystemSession
 import org.eclipse.aether.repository.LocalRepository
 import org.eclipse.aether.repository.RemoteRepository
 
-
 /**
  * A helper to boot the repository system and a repository system session.
  */
 class Booter {
+
+    /**
+     * Default artifact repository
+     */
+    private static final String DEFAULT_REPO = "data/repo"
+
+    /**
+     *
+     * @return
+     */
     public static RepositorySystem newRepositorySystem() {
         return ManualRepositorySystemFactory.newRepositorySystem();
-        // return org.eclipse.aether.examples.guice.GuiceRepositorySystemFactory.newRepositorySystem();
-        // return org.eclipse.aether.examples.sisu.SisuRepositorySystemFactory.newRepositorySystem();
-        // return org.eclipse.aether.examples.plexus.PlexusRepositorySystemFactory.newRepositorySystem();
     }
 
+    /**
+     *
+     * @param system
+     * @return
+     */
     public static DefaultRepositorySystemSession newRepositorySystemSession(RepositorySystem system) {
+        return newRepositorySystemSession(system, new File(DEFAULT_REPO));
+    }
+
+    /**
+     *
+     * @param system
+     * @param repository
+     * @return
+     */
+    public static DefaultRepositorySystemSession newRepositorySystemSession(RepositorySystem system, File repository) {
         DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
-
-        LocalRepository localRepo = new LocalRepository("target/local-repo");
+        LocalRepository localRepo = new LocalRepository(repository);
         session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
-
-//            session.setTransferListener( new ConsoleTransferListener() );
-//            session.setRepositoryListener( new ConsoleRepositoryListener() );
-
-        // uncomment to generate dirty trees
-        // session.setDependencyGraphTransformer( null );
-
         return session;
     }
 
+    /**
+     *
+     * @param system
+     * @param session
+     * @return
+     */
     public static List<RemoteRepository> newRepositories(RepositorySystem system, RepositorySystemSession session) {
         return new ArrayList<RemoteRepository>(Arrays.asList(newCentralRepository()));
     }
 
+    /**
+     *
+     * @return
+     */
     private static RemoteRepository newCentralRepository() {
         return new RemoteRepository.Builder("central", "default", "http://central.maven.org/maven2/").build();
+    }
+
+    /**
+     * 
+     * @param url
+     * @return
+     */
+    private static RemoteRepository newCentralRepository(String url){
+        return new RemoteRepository.Builder("central", "default", url).build();
     }
 
 }
