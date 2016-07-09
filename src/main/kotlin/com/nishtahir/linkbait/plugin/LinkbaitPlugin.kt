c7a216b1b3b3f3c3c35a34f75fe2edf347d7d0fd
Nish Tahir <nishtahir@outlook.com>
2016-07-09 17:14:09 +0300

    Add readme and ci

---
+++ src/main/kotlin/com/nishtahir/linkbait/plugin/LinkbaitPlugin.kt
@@ -0,0 +1,11 @@
+package com.nishtahir.linkbait.plugin
+
+import ro.fortsoft.pf4j.Plugin
+import ro.fortsoft.pf4j.PluginWrapper
+
+/**
+ * Created by nish on 7/9/16.
+ */
+class LinkbaitPlugin : Plugin {
+    constructor(wrapper: PluginWrapper?) : super(wrapper)
+}
